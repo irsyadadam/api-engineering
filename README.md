3f4aab760ce98147b318e1c62105f8a48a4f6cd4
irsyadadam <59972182+irsyadadam@users.noreply.github.com>
2021-11-15 14:09:18 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+### server_flask_app.py:
+flask app, single endpoint with meshid and batch request param, returns every single doc associated with the mesh id
+
+### libs:
+functions that are utilized in server_flask_app.py
+  - elasticsearch_lookup.py
+  - extract_mesh.py
+  - create_mesh_headings.py
---
+++ server_flask_app.py
@@ -0,0 +1,98 @@
+from flask import Flask, request, jsonify
+import requests
+import ast
+import json
+
+import libs.extract_mesh as em
+import libs.elasticsearch_lookup as el
+
+"""
+Notes:
+
+@shell:
+conda env list
+
+On virtual environment, only library installed is flask + dependencies
+    @shell:
+    conda list
+
+Everything is to run on shell. To run app:
+    @shell:
+    export FLASK_APP=flask_app.py 
+    flask run 
+
+NOTE: Make sure you aeete in directory of flask app to export it!!
+
+---------------------------------
+
+client -- requests library --> flask server <-- http response --> view function {return data}
+
+"""
+
+#init the app
+app = Flask(__name__)
+#app.debug = True
+
+#import the mesh data in dict
+print("---loading mesh terms---")
+mesh_data = em.read_mesh_data("mesh_data.xml")
+mesh_id_to_term_map = em.extract_mesh_term_and_id(mesh_data)
+assert mesh_id_to_term_map["D009202"] == "Cardiomyopathies"
+print("----------done----------")
+
+def check_mesh(mesh_term:str) -> bool:
+    """
+    check if a mesh term is in mesh dict
+    @param the mesh_term
+    @returns bool
+    """
+    return mesh_term in mesh_id_to_term_map
+
+def check_docs(docs:int) -> bool:
+    """
+    checks batch request
+    @param int is the batch request
+    @return bool if valid request
+    """
+    return docs >= 1 and docs <= 500    
+
+def batch_elasticsearch(mesh_request:str, doc_request:int) -> dict:
+    """
+    returns a number of documents from the elasticsearch query
+    @param mesh_request is the mesh tmer
+    @param docs is the batch request
+    @returns a list of docs
+    """
+    return el.elasticsearch_query(mesh_id_to_term_map[mesh_request], doc_request)
+
+
+#valid request is http://34.217.174.15:5000/api/data/pubmed_central?mesh=100&docs=10
+@app.route("/api/data/pubmed_central/", methods=["GET"])
+def mesh_docs():
+    """
+    returns data associated with mesh term
+    @param mesh term specified by enpoint
+    @param docs is the batch request specified by the endpoint
+    @returns the document mapping associated with the mesh term
+    """
+    #parse request
+    mesh_request = request.args.get("mesh")
+    doc_request = request.args.get("docs")
+
+    #checks if valid mesh term and batch request
+    if check_mesh(mesh_request) and check_docs(int(doc_request)):
+        #return data using elasticsearch
+        return jsonify(batch_elasticsearch(mesh_request, int(doc_request)))
+
+    else:
+        #return str("sad :(")
+        return "invalid mesh term/batch request"
+
+#if the request didn't go through
+@app.errorhandler(404)
+def error(error):
+    return "invalid url/endpoint, request url: http://34.217.174.15:5000/api/data/pubmed_central", 404
+
+if __name__ == "__main__":
+    #run python file while app is running to run debug mode
+    app.run(host='0.0.0.0', port=5000)
